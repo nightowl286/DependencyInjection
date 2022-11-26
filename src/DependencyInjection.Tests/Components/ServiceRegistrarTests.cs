ade48ae749bee512139020988d506c3f87862cd1
Nightowl <79215414+nightowl286@users.noreply.github.com>
2022-11-26 00:00:07 +0000

    Added locking tests.

--- src/DependencyInjection.Tests/Components/ServiceRegistrarTests.cs
+++ src/DependencyInjection.Tests/Components/ServiceRegistrarTests.cs
@@ -1,4 +1,5 @@
 ﻿using Moq;
+using TNO.Common.Locking;
 using TNO.DependencyInjection.Abstractions;
 using TNO.DependencyInjection.Abstractions.Components;
 using TNO.DependencyInjection.Components;
@@ -35,6 +36,109 @@ namespace TNO.DependencyInjection.Tests.Components
       }
 
       #region Tests
+      #region Locking
+      [TestMethod]
+      public void TryLock_WhileUnlocked_ReturnsTrue()
+      {
+         // Pre-Act Assert
+         Assert.That.IsInconclusiveIf(_sut.IsLocked);
+
+         // Act
+         bool result = _sut.TryLock(out ReferenceKey? key);
+
+         // Assert
+         Assert.IsTrue(result);
+         Assert.IsTrue(_sut.IsLocked);
+         Assert.IsNotNull(key);
+      }
+
+      [TestMethod]
+      public void TryUnlock_WhileLockedWithCorrectKey_ReturnsTrue()
+      {
+         // Arrange
+         ReferenceKey key = LockSut();
+
+         // Act
+         bool result = _sut.TryUnlock(key);
+
+         // Assert
+         Assert.IsTrue(result);
+         Assert.IsFalse(_sut.IsLocked);
+      }
+
+      [TestMethod]
+      public void TryLock_WhileLocked_ReturnsFalse()
+      {
+         // Arrange
+         _ = LockSut();
+
+         // Act
+         bool result = _sut.TryLock(out ReferenceKey? key);
+
+         // Assert
+         Assert.IsFalse(result);
+         Assert.IsTrue(_sut.IsLocked);
+         Assert.IsNull(key);
+      }
+
+      [TestMethod]
+      public void TryUnlock_WhileUnlocked_ReturnsFalse()
+      {
+         // Arrange
+         ReferenceKey key = LockSut();
+         bool arrangeResult = _sut.TryUnlock(key);
+
+         // Arrange Assert
+         Assert.That.IsInconclusiveIfNot(arrangeResult);
+         Assert.That.IsInconclusiveIf(_sut.IsLocked);
+
+         // Act
+         bool result = _sut.TryUnlock(key);
+
+         // Assert
+         Assert.IsFalse(result);
+         Assert.IsFalse(_sut.IsLocked);
+      }
+
+      [TestMethod]
+      public void TryLock_AfterLockAndUnlock_ReturnsUniqueKey()
+      {
+         // Pre-Arrange Assert
+         Assert.That.IsInconclusiveIf(_sut.IsLocked);
+
+         // Arrange
+         ReferenceKey key1 = LockSut();
+         UnlockSut(key1);
+
+         // Act
+         bool result = _sut.TryLock(out ReferenceKey? key2);
+
+         // Assert
+         Assert.That.IsInconclusiveIfNot(result);
+         Assert.That.IsInconclusiveIfNot(_sut.IsLocked);
+
+         Assert.AreNotSame(key1, key2);
+      }
+
+      [TestMethod]
+      public void TryUnlock_WithWrongKey_ReturnsFalse()
+      {
+         // Arrange
+         ReferenceKey badKey = new ReferenceKey();
+         ReferenceKey goodKey = LockSut();
+
+         // Arrange Assert
+         Assert.That.IsInconclusiveIf(ReferenceEquals(badKey, goodKey));
+
+         // Act
+         bool result = _sut.TryUnlock(badKey);
+
+         // Assert
+         Assert.IsFalse(result);
+         Assert.IsTrue(_sut.IsLocked);
+      }
+      #endregion
+
       #region Instance
       [DynamicData(nameof(GetAllRegistrationModes))]
       [TestMethod]
@@ -316,6 +420,26 @@ namespace TNO.DependencyInjection.Tests.Components
          _facadeMock.VerifyOnce(f => f.CreateScope(registrationMode));
       }
       #endregion
+      #endregion
+
+      #region Helpers
+      private ReferenceKey LockSut()
+      {
+         bool result = _sut.TryLock(out ReferenceKey? key);
+
+         Assert.That.IsInconclusiveIf(key is null);
+         Assert.That.IsInconclusiveIfNot(result);
+         Assert.That.IsInconclusiveIfNot(_sut.IsLocked);
+
+         return key;
+      }
+      private void UnlockSut(ReferenceKey key)
+      {
+         bool result = _sut.TryUnlock(key);
+
+         Assert.That.IsInconclusiveIfNot(result);
+         Assert.That.IsInconclusiveIf(_sut.IsLocked);
+      }
       #endregion
    }
 }
